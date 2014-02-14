001f48b9e962204b6984e261259f8651716ad0e5
Nicolas Duval <nduval.dev@gmail.com>
2014-02-14 22:00:47 +0100

    Update cdl.rb

--- lib/cdl.rb
+++ lib/cdl.rb
@@ -1,8 +1,7 @@
-
 require_relative "cdl/version.rb"
 require_relative "cdl/edl.rb"
  
-cdl = Cdl::Edl.new("../fixtures/CDL_V1_092612.edl")
+#cdl = Cdl::Edl.new("../fixtures/CDL_V1_092612.edl")
 # cdl.read_file
 # cdl.to_hash
 
